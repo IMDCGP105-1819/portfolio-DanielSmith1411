67bcb532d8ba348a03129223300606d6e8c8b9a7
DanielSmith1411 <32357121+DanielSmith1411@users.noreply.github.com>
2018-09-27 12:04:40 +0100

    Initial Commit
    Created a folder for the weekly tasks in the Introduction to Programming module and added the first task.

---
+++ Introduction to Programming Weekly Tasks/ex1.py
@@ -0,0 +1,9 @@
+q = "Isn't this a thrill?"
+#A bunch of print statements seemed a bit dull so I stuck in a variable.
+print("Hello World!")
+print('This is exciting.')
+print(q)
+print("...")
+print("I said '" + q + "'")
+print("...")
+print('No, you are right, it is "boring".')
